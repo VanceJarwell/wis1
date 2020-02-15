083ca30089e150a56edb88778a853ff47b16160f
Vance <vancejarwell@gmail.com>
2020-02-15 17:30:52 +0800

    wis1

---
+++ phpexerciselastone.php
@@ -0,0 +1,16 @@
+<?php
+ while(true) {
+    $inputs = explode(' ', trim(fgets(STDIN))); 
+    if (!is_array($inputs) || count($inputs) < 2) {
+        exit;
+    } 
+    $a = $inputs[0];
+    $b = $inputs[1]; 
+    $d = numDigits($a + $b);
+    echo("Digit number of sum of two given integers: ");
+    echo $d . "\n";
+} 
+function numDigits($n) {
+    return (int)(log10($n) + 1);
+}
+?>
